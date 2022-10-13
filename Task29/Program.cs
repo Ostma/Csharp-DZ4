5e204f1f1dd5e5d7659a178f53d91868f4131fab
MaxO <ostma@mail.ru>
2022-10-13 22:54:01 +0500

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// ввод элеменетов через консоль (через запятую. использовать метод string.Split())
+
+
+Console.WriteLine("Введите 8 цифр, разделяя их запятыми: ");
+string array = Console.ReadLine();
+
+string[] massiv = array.Split(",");
+
+if (massiv.Length == 8)
+{
+    for (int i = 0; i < massiv.Length; i++)
+    Console.Write($"{massiv[i]} ");
+}
+else
+Console.WriteLine("Вы ввели не восемь цифр");
+// foreach (string sub in massiv)
+// {
+//     Console.WriteLine($"Substring: {sub}");
+// }
